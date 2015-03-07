03bb55c1e6183897793762801f750d1958facb70
D. Garcia <dario.garcia@samsung5>
2015-03-07 19:32:51 -0300

    Loading templates

--- src/main/javascript/web/js/templates.js
+++ src/main/javascript/web/js/templates.js
@@ -1,3 +1,37 @@
+Ember.TEMPLATES["loading"] = Ember.HTMLBars.template((function() {
+  return {
+    isHTMLBars: true,
+    blockParams: 0,
+    cachedFragment: null,
+    hasRendered: false,
+    build: function build(dom) {
+      var el0 = dom.createTextNode("Loading section ...");
+      return el0;
+    },
+    render: function render(context, env, contextualElement) {
+      var dom = env.dom;
+      dom.detectNamespace(contextualElement);
+      var fragment;
+      if (env.useFragmentCache && dom.canClone) {
+        if (this.cachedFragment === null) {
+          fragment = this.build(dom);
+          if (this.hasRendered) {
+            this.cachedFragment = fragment;
+          } else {
+            this.hasRendered = true;
+          }
+        }
+        if (this.cachedFragment) {
+          fragment = dom.cloneNode(this.cachedFragment, true);
+        }
+      } else {
+        fragment = this.build(dom);
+      }
+      return fragment;
+    }
+  };
+}()));
+
 Ember.TEMPLATES["otherPaths"] = Ember.HTMLBars.template((function() {
   return {
     isHTMLBars: true,
@@ -474,6 +508,40 @@ Ember.TEMPLATES["users/edit"] = Ember.HTMLBars.template((function() {
       inline(env, morph3, context, "input", [], {"value": get(env, context, "model.password")});
       element(env, element1, context, "action", ["save"], {});
       element(env, element2, context, "action", ["remove"], {});
+      return fragment;
+    }
+  };
+}()));
+
+Ember.TEMPLATES["users/loading"] = Ember.HTMLBars.template((function() {
+  return {
+    isHTMLBars: true,
+    blockParams: 0,
+    cachedFragment: null,
+    hasRendered: false,
+    build: function build(dom) {
+      var el0 = dom.createTextNode("Loading user ....");
+      return el0;
+    },
+    render: function render(context, env, contextualElement) {
+      var dom = env.dom;
+      dom.detectNamespace(contextualElement);
+      var fragment;
+      if (env.useFragmentCache && dom.canClone) {
+        if (this.cachedFragment === null) {
+          fragment = this.build(dom);
+          if (this.hasRendered) {
+            this.cachedFragment = fragment;
+          } else {
+            this.hasRendered = true;
+          }
+        }
+        if (this.cachedFragment) {
+          fragment = dom.cloneNode(this.cachedFragment, true);
+        }
+      } else {
+        fragment = this.build(dom);
+      }
       return fragment;
     }
   };
