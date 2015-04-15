9a0664229b8459a7448a38126649ef361dbd5060
D. Garcia <dario.garcia@samsung5>
2015-04-15 02:05:23 -0300

    Fixed logout

--- src/main/javascript/web/js/router.js
+++ src/main/javascript/web/js/router.js
@@ -36,6 +36,20 @@ App.ApplicationRoute = Ember.Route.extend({
         console.log(transition);
         return this._super(error, transition);
       }
+    },
+    logout: function() {
+      var self = this;
+      Ember.$.post("/j_logout", {}).then(
+        function(response) {
+          var loginController = self.controllerFor('login');
+          loginController.set('authenticated', null);
+          self.transitionTo('login');
+        },
+        function(response){
+          console.log("Error logging out");
+          console.log(response);
+        }
+      );
     }
   }
 });
