ef53a3a14c84e0875de527889476cd53a650a9fc
Dario L Garcia <dario.garcia@10pines.com>
2016-10-27 22:54:22 -0300

    Extract search to action

---
+++ src/main/java/convention/action/FindProceduresAction.java
@@ -0,0 +1,47 @@
+package convention.action;
+
+import ar.com.kfgodel.appbyconvention.operation.api.ApplicationOperation;
+import ar.com.kfgodel.dependencies.api.DependencyInjector;
+import ar.com.kfgodel.diamond.api.types.reference.ReferenceOf;
+import ar.com.kfgodel.nary.api.Nary;
+import ar.com.kfgodel.proact.persistent.filters.procedures.ProceduresByTextPortionOrdByName;
+import convention.persistent.Procedure;
+import convention.rest.api.tos.ProcedureFilterTo;
+import convention.rest.api.tos.ProcedureTo;
+
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * This type represents the search procedures action that can be used from the frontend
+ * Created by kfgodel on 27/10/16.
+ */
+public class FindProceduresAction implements Function<ProcedureFilterTo, List<Procedure>> {
+
+  private static final Type LIST_OF_PROCEDURES_TO = new ReferenceOf<List<ProcedureTo>>() {
+  }.getReferencedType();
+
+  private DependencyInjector injector;
+
+  public static FindProceduresAction create(DependencyInjector injector) {
+    FindProceduresAction action = new FindProceduresAction();
+    action.injector = injector;
+    return action;
+  }
+
+  @Override
+  public List<Procedure> apply(ProcedureFilterTo filter) {
+    String searchText = filter.getSearchText();
+    List<Procedure> operationResult = createOperation()
+      .insideASession()
+      .applying(ProceduresByTextPortionOrdByName.create(Nary.ofNullable(searchText)))
+      .convertTo(LIST_OF_PROCEDURES_TO);
+    return operationResult;
+  }
+
+  private ApplicationOperation createOperation() {
+    return ApplicationOperation.createFor(injector);
+  }
+
+}
--- src/main/java/convention/rest/api/MessageResource.java
+++ src/main/java/convention/rest/api/MessageResource.java
@@ -2,19 +2,16 @@ package convention.rest.api;
 
 import ar.com.kfgodel.appbyconvention.operation.api.ApplicationOperation;
 import ar.com.kfgodel.dependencies.api.DependencyInjector;
-import ar.com.kfgodel.diamond.api.types.reference.ReferenceOf;
-import ar.com.kfgodel.nary.api.Nary;
-import ar.com.kfgodel.proact.persistent.filters.procedures.ProceduresByTextPortionOrdByName;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import convention.action.FindProceduresAction;
+import convention.persistent.Procedure;
 import convention.rest.api.tos.ProcedureFilterTo;
-import convention.rest.api.tos.ProcedureTo;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
-import java.lang.reflect.Type;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -30,29 +27,23 @@ public class MessageResource {
   @Inject
   private DependencyInjector appInjector;
 
-  private static final Type LIST_OF_PROCEDURES_TO = new ReferenceOf<List<ProcedureTo>>() {
-  }.getReferencedType();
-
   @POST
   public Object getAllEntities(Map<String, Object> messageContent) {
     ObjectMapper objectMapper = new ObjectMapper();
     ProcedureFilterTo filter = objectMapper.convertValue(messageContent, ProcedureFilterTo.class);
 
-    String searchText = filter.getSearchText();
-    Object operationResult = createOperation()
-      .insideASession()
-      .applying(ProceduresByTextPortionOrdByName.create(Nary.ofNullable(searchText)))
-      .convertTo(LIST_OF_PROCEDURES_TO);
+    FindProceduresAction action = FindProceduresAction.create(appInjector);
+    List<Procedure> actionResult = action.apply(filter);
 
     JavaType expectedType;
-    if (operationResult instanceof Collection) {
+    if (actionResult instanceof Collection) {
       // Use array like
       expectedType = objectMapper.getTypeFactory().constructCollectionType(List.class, Object.class);
     } else {
       // Normal object, use a hash
       expectedType = objectMapper.getTypeFactory().constructMapType(Map.class, String.class, Object.class);
     }
-    Object jsonized = objectMapper.convertValue(operationResult, expectedType);
+    Object jsonized = objectMapper.convertValue(actionResult, expectedType);
     return jsonized;
   }
 
